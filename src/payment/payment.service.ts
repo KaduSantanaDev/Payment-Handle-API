159d23352535c76fc8454aba453e90d8bcd6157a
Kadu <kaduzera.ks@gmail.com>
2023-12-07 17:07:56 -0300

    Add boleto for payment methods

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -4,7 +4,7 @@ import { Stripe } from 'stripe'
 @Injectable()
 export class PaymentService {
   private stripe = new Stripe('sk_test_51My4ZJAtJK3MHkH3LteOSfCkfrDZd2BZAUnro0ncwzHwZt3WWEusLZYkGU3BJH0YSB6IQuHy3N6r0MUK3nXawp4i00FUdhAfH7', {apiVersion: '2023-10-16'})
-  
+
   async generatePayment() {
     try {
       const session = await this.stripe.checkout.sessions.create({
@@ -15,30 +15,31 @@ export class PaymentService {
             quantity: 1
           }
         ],
+        payment_method_types: ['card', 'boleto'],
         mode: 'payment'
       })
-      
+
       return {'url': session.url, 'id': session.id}
     } catch (error) {
       console.error(error)
       throw new HttpException('An error occured', HttpStatus.INTERNAL_SERVER_ERROR)
     }
-    
+
   }
-  
+
   async retrieveSession(sessionId) {
     try {
       const session = await this.stripe.checkout.sessions.retrieve(sessionId)
-      
+
       return {'status': session.payment_status}
     } catch (error) {
       console.error(error)
     }
   }
-  
-  
-  
-  
+
+
+
+
 
 }
 
